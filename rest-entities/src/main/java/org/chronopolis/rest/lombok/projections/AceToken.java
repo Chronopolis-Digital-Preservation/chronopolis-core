3d6de489e46e95381f867f921497d269e21246b9
Michael Ritter <shake@umiacs.umd.edu>
2019-09-17 17:09:19 -0400

    Query projections

---
+++ rest-entities/src/main/java/org/chronopolis/rest/lombok/projections/AceToken.java
@@ -0,0 +1,45 @@
+package org.chronopolis.rest.lombok.projections;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * @author shake
+ */
+@Getter
+@ToString
+public class AceToken {
+    @ToString.Include private final Long id;
+    @ToString.Include private final Long bagId;
+    @ToString.Include private final String filename;
+    private final Long round;
+    private final String imsHost;
+    private final String imsService;
+    private final String algorithm;
+    private final String proof;
+    private final Date createdAt;
+
+    @QueryProjection
+    public AceToken(Long id,
+                    Long bagId,
+                    Long round,
+                    String imsHost,
+                    String imsService,
+                    String algorithm,
+                    String proof,
+                    Date createdAt,
+                    String filename) {
+        this.id = id;
+        this.bagId = bagId;
+        this.round = round;
+        this.imsHost = imsHost;
+        this.imsService = imsService;
+        this.algorithm = algorithm;
+        this.proof = proof;
+        this.createdAt = createdAt;
+        this.filename = filename;
+    }
+}
---
+++ rest-entities/src/main/java/org/chronopolis/rest/lombok/projections/ReplicationView.java
@@ -0,0 +1,73 @@
+package org.chronopolis.rest.lombok.projections;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.ToString;
+import org.chronopolis.rest.models.enums.ReplicationStatus;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author shake
+ */
+@Getter
+@ToString
+public class ReplicationView {
+    private final Long id;
+    private final ZonedDateTime createdAt;
+    private final ZonedDateTime updatedAt;
+    private final ReplicationStatus status;
+    private final String bagLink;
+    private final String tokenLink;
+    private final String protocol;
+    private final String node;
+    private final CompleteBag bag;
+    private String receivedTagFixity;
+    private String receivedTokenFixity;
+
+    @QueryProjection
+    public ReplicationView(Long id,
+                           ZonedDateTime createdAt,
+                           ZonedDateTime updatedAt,
+                           ReplicationStatus status,
+                           String bagLink,
+                           String tokenLink,
+                           String protocol,
+                           String node,
+                           CompleteBag bag) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.status = status;
+        this.bagLink = bagLink;
+        this.tokenLink = tokenLink;
+        this.protocol = protocol;
+        this.node = node;
+        this.bag = bag;
+    }
+
+    @QueryProjection
+    public ReplicationView(Long id,
+                           ZonedDateTime createdAt,
+                           ZonedDateTime updatedAt,
+                           ReplicationStatus status,
+                           String bagLink,
+                           String tokenLink,
+                           String protocol,
+                           String receivedTagFixity,
+                           String receivedTokenFixity,
+                           String node,
+                           CompleteBag bag) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.status = status;
+        this.bagLink = bagLink;
+        this.tokenLink = tokenLink;
+        this.protocol = protocol;
+        this.receivedTagFixity = receivedTagFixity;
+        this.receivedTokenFixity = receivedTokenFixity;
+        this.node = node;
+        this.bag = bag;
+    }
+}
