7fdf714db21cb6c9a273cb6b37efa1dcedfc643b
Michael Ritter <shake@umiacs.umd.edu>
2019-09-30 16:41:23 -0400

    Replace SerializerContext

---
+++ rest-entities/src/test/java/org/chronopolis/rest/entities/serializers/SerializerContext.java
@@ -0,0 +1,16 @@
+package org.chronopolis.rest.entities.serializers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shake
+ */
+@SpringBootApplication
+public class SerializerContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerializerContext.class);
+    }
+
+}
