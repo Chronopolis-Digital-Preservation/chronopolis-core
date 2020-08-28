c3c320f39ebf70a83290791541e3cc00a654c4c4
longshousitu <longshousitu@localhost>
2020-08-28 13:50:20 -0700

    Move system properties for test to application.yml.

--- ingest-rest/src/test/java/org/chronopolis/ingest/IngestTest.java
+++ ingest-rest/src/test/java/org/chronopolis/ingest/IngestTest.java
@@ -1,9 +1,5 @@
 package org.chronopolis.ingest;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.springframework.test.context.ActiveProfiles;
-
 /**
  * Class to force certain properties to be set so that our tests are done
  * in a consistent manner.
@@ -11,23 +7,5 @@ import org.springframework.test.context.ActiveProfiles;
  * Created by shake on 3/26/15.
  */
 public class IngestTest {
-
-    private static final String AJP_ENABLED = "ingest.ajp.enabled";
-    private static final String FLYWAY_ENABLED = "spring.flyway.enabled";
-    private static final String DATASOURCE_INITIALIZE = "spring.datasource.initialize";
-
-    @BeforeClass
-    public static void init() {
-        System.setProperty(AJP_ENABLED, "false");
-        System.setProperty(FLYWAY_ENABLED, "false");
-        System.setProperty(DATASOURCE_INITIALIZE, "false");
-    }
-
-    @AfterClass
-    public static void clearProperties() {
-        System.clearProperty(AJP_ENABLED);
-        System.clearProperty(FLYWAY_ENABLED);
-        System.clearProperty(DATASOURCE_INITIALIZE);
-    }
 
 }
